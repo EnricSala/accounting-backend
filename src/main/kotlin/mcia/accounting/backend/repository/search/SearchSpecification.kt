d315988905735bd1e9fe0bd0bb48017adc4ff224
Enric Sala <sinh32.enric@gmail.com>
2017-11-10 15:52:28 +0100

    Simplify and cleanup SearchSpecification code

--- src/main/kotlin/mcia/accounting/backend/repository/search/SearchSpecification.kt
+++ src/main/kotlin/mcia/accounting/backend/repository/search/SearchSpecification.kt
@@ -31,31 +31,30 @@ object SearchSpecification {
     inline fun <reified T : Any> of(criteria: SearchCriteria): Specification<T> =
             Specification { root: Root<T>, _: CriteriaQuery<*>, builder: CriteriaBuilder ->
                 val type = typeOf<T>(criteria.key)
-                val path = unpackLast(root, criteria.key)
-                val prop = lastProperty(criteria.key)
+                val (prop, path) = findLastProperty(criteria.key, root)
                 when (criteria.operation) {
                     EQUAL -> when (type) {
                         String::class -> builder.equal(path.get<String>(prop), criteria.value)
-                        Date::class -> throw IllegalArgumentException("dates do not support equal")
+                        Date::class -> builder.equal(path.get<Date>(prop), criteria.date())
                         Boolean::class -> builder.equal(path.get<Boolean>(prop), criteria.boolean())
                         else -> builder.equal(path.get<String>(prop), criteria.value)
                     }
                     NOT_EQUAL -> when (type) {
                         String::class -> builder.notEqual(path.get<String>(prop), criteria.value)
-                        Date::class -> throw IllegalArgumentException("dates do not support not_equal")
+                        Date::class -> builder.notEqual(path.get<Date>(prop), criteria.date())
                         Boolean::class -> builder.notEqual(path.get<Boolean>(prop), criteria.boolean())
                         else -> builder.notEqual(path.get<String>(prop), criteria.value)
                     }
                     GREATER_THAN -> when (type) {
                         String::class -> builder.greaterThanOrEqualTo(path.get<String>(prop), criteria.value)
                         Date::class -> builder.greaterThanOrEqualTo(path.get<Date>(prop), criteria.date())
-                        Boolean::class -> throw IllegalArgumentException("booleans do not support greater_than")
+                        Boolean::class -> throw IllegalArgumentException("greater_than does not support $type")
                         else -> builder.greaterThanOrEqualTo(path.get<String>(prop), criteria.value)
                     }
                     LESS_THAN -> when (type) {
                         String::class -> builder.lessThanOrEqualTo(path.get<String>(prop), criteria.value)
                         Date::class -> builder.lessThanOrEqualTo(path.get<Date>(prop), criteria.date())
-                        Boolean::class -> throw IllegalArgumentException("booleans do not support less_than")
+                        Boolean::class -> throw IllegalArgumentException("less_than does not support $type")
                         else -> builder.lessThanOrEqualTo(path.get<String>(prop), criteria.value)
                     }
                     CONTAINS -> when (type) {
@@ -63,7 +62,7 @@ object SearchSpecification {
                             val upperCase = builder.upper(path.get<String>(prop))
                             builder.like(upperCase, "%${criteria.value.toUpperCase()}%")
                         }
-                        else -> throw IllegalArgumentException("contains can only be used with strings")
+                        else -> throw IllegalArgumentException("contains does not support $type")
                     }
                     else -> throw UnsupportedOperationException("unsupported search operation")
                 }
@@ -79,16 +78,12 @@ object SearchSpecification {
         return type
     }
 
-    fun <T> unpackLast(root: Root<T>, key: String): Path<T> {
-        if (!key.contains(PROPERTY_DELIMITER))
-            return root
-        var result: Path<T>? = null
+    fun findLastProperty(key: String, root: Root<*>): Pair<String, Path<*>> {
         val parts = key.split(PROPERTY_DELIMITER)
+        var path: Path<*> = root
         for (i in 0..(parts.size - 2))
-            result = (result ?: root).get<T>(parts[i])
-        return result!!
+            path = path.get<Any>(parts[i])
+        return parts.last() to path
     }
-
-    fun lastProperty(key: String) = key.substringAfterLast(PROPERTY_DELIMITER)
 
 }
